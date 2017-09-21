01fe727d44e265b99fec9144f1b2e9f66c7c254c
Oscar Fonts <oscar.fonts@gmail.com>
2017-09-21 14:26:18 +0200

    Write attachments to disk

--- couchdb_change_notifier.js
+++ couchdb_change_notifier.js
@@ -1,29 +1,72 @@
 const events = require('events')
 const request = require('request')
 
-const CouchdbChangeNotifier = function({db, user, password}) {
-  const eventEmitter = new events.EventEmitter()
-  let last_seq = 0
+const CouchdbChangeNotifier = function({db, user, password, since=0, interval=10}) {
+  const event = new events.EventEmitter()
+  let last_seq = since
+
+  const noDesignDocuments = (item) => !item.id.startsWith('_design/')
 
   const pollForChanges = () => {
-    request.get(db + '_changes/', {
+    request.get({
+      baseUrl: db,
+      url: '_changes',
+      qs: {
+        since: last_seq,
+        include_docs: true,
+        attachments: true
+      },
       auth: {
         user: user,
         pass: password
         // bearer: token
-      }
+      },
+      json: true
     }, (error, response, body) => {
-      const message = JSON.parse(body)
-      message.results.map((result) => eventEmitter.emit('change', result))
-      last_seq = message.last_seq
-    }).on('error', (err) => {
-      console.log(err)
+      if (error) {
+        onError(error)
+      } else {
+        body.results.filter(noDesignDocuments).map((item) => item.deleted ? onDelete(item) : onChange(item))
+        last_seq = body.last_seq
+        onPoll(last_seq)
+        // Schedule next query only after the previous one has been completely processed
+        setTimeout(pollForChanges, interval * 1000)
+      }
     })
   }
 
-  pollForChanges()
+  const onPoll = (seq) => {
+    event.emit('poll', seq)
+  }
 
-  return eventEmitter
+  const onError = (error) => {
+    event.emit('error', error)
+  }
+
+  const onChange = (item) => {
+    const attachments = item.doc._attachments
+    const doc = Object.assign({}, item.doc)
+    delete doc._rev
+    delete doc._attachments
+
+    if(attachments) {
+      const arr = Object.keys(attachments).map(key => attachments[key])
+      doc.attachments = arr.map((attachment) => ({
+        content_type: attachment.content_type,
+        data: attachment.data ? new Buffer(attachment.data, 'base64') : undefined
+      }))
+    }
+
+    event.emit('change', doc)
+  }
+
+  const onDelete = (item) => {
+    const id = item.doc._id
+    event.emit('delete', id)
+  }
+
+  pollForChanges()
+  return event
 }
 
 module.exports = CouchdbChangeNotifier
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 require('dotenv-safe').load()
+const fs = require('fs')
 
 const CouchdbChangeNotifier = require('./couchdb_change_notifier')
 
@@ -7,8 +8,24 @@ const notifier = new CouchdbChangeNotifier({
   db: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD
+  // seq: N
 })
 
-const listener = (document) => console.log(document)
+notifier.on('change', (doc) => console.log("CHANGED " + doc._id))
+notifier.on('delete', (id) => console.log("DELETED " + id))
+notifier.on('poll', (seq) => console.log("LAST_SEQ " + seq))
+notifier.on('error', (cause) => console.error(cause))
 
-notifier.on('change', listener)
+notifier.on('change', (doc) => {
+  if ('attachments' in doc) {
+    const extensions = {
+      'image/jpeg': 'jpg',
+      'image/png': 'png'
+    }
+    const extension = extensions[doc.attachments[0].content_type]
+    const filename = doc._id + '.' + extension
+    const data = doc.attachments[0].data
+    console.log('  *** Writing file ' + filename + ' ***  ')
+    fs.writeFileSync(filename, data)
+  }
+})
