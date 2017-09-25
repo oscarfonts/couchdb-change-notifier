802aba1c06c5edd1ace39c078543627ead41e9f2
Oscar Fonts <oscar.fonts@gmail.com>
2017-09-25 10:37:04 +0200

    Control last sequence

--- example.js
+++ example.js
@@ -1,21 +1,29 @@
 #!/usr/bin/env node
 require('dotenv-safe').load()
 const fs = require('fs')
-
 const CouchdbChangeNotifier = require('./couchdb_change_notifier')
 
-const notifier = new CouchdbChangeNotifier({
-  db: process.env.DB_NAME,
+// Read last_seq
+const since = fs.existsSync('_since') ? fs.readFileSync('_since') : 0
+console.log('Reading changes from SEQ ' + since)
+
+// Start notifier
+const notifier = new CouchdbChangeNotifier(process.env.DB_NAME, {
+  since: since,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD
-  // seq: N
 })
 
-notifier.on('change', (doc) => console.log("CHANGED " + doc._id))
-notifier.on('delete', (id) => console.log("DELETED " + id))
-notifier.on('poll', (seq) => console.log("LAST_SEQ " + seq))
+// Log events to console
+notifier.on('change', (doc) => console.log('CHANGED ' + doc._id))
+notifier.on('delete', (id) => console.log('DELETED ' + id))
+notifier.on('poll', (seq) => console.log('LAST_SEQ ' + seq))
 notifier.on('error', (cause) => console.error(cause))
 
+// Save last_seq
+notifier.on('poll', (seq) => fs.writeFileSync('_since', seq))
+
+// Write images
 notifier.on('change', (doc) => {
   if ('attachments' in doc) {
     const extensions = {
